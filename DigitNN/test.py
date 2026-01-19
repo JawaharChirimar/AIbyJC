a9f4c28b22a780ff150734db1a9c8d44982d9b95
Jawahar Chirimar <jc@onemanagement.com>
2026-01-19 01:54:06 -0500

    Added test script

--- DigitNN/test.py
+++ DigitNN/test.py
@@ -4,7 +4,9 @@ import sys
 sys.path.insert(0, str(Path.home() / "Development/AIbyJC/DigitNN"))
 from DigitsExtractor import detect_digits_with_contours, detect_background_color_contours
 
-img = cv2.imread(str(Path.home() / "Development/AIbyJC/DigitNN/data/input/hw4.png"), cv2.IMREAD_GRAYSCALE)
+img_path = Path(__file__).parent / "data/input/hw4.png"
+
+img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
 
 background_mean, foreground_mean = detect_background_color_contours(img)
 print(f"Background: {background_mean}, Foreground: {foreground_mean}")
